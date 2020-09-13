12a42886782b2d1ff37a1273717d68e0107d5756
willycoffee <67596846+willycoffee@users.noreply.github.com>
2020-09-13 03:50:58 -0400

    changed still wip to coming soon
    cause thats basically true xd

--- levelComments.js
+++ levelComments.js
@@ -4,19 +4,15 @@ const atob = require("atob");
 function getLevelComments(levelID, page) {
   req.post('http://boomlings.com/database/getGJComments21.php', {form: {secret: "Wmfd2893gb7", levelID: levelID, page: page,}}, (err, res, body) => {
     let response = body.toString()
-    response = response.split('~')
-
-    let commentInfo = {
-      comment: atob(response[1])
-    }
+    comments = response.split('~')
 
     if (body == '-1') {
       console.log('Not found.')
     } else {
-      // response.forEach((item, i) => {
-      //   console.log(`${i}: ${item}`)
-      // })
-      console.log('No, not yet. Still work in progress.')
+      // comments.forEach((x) => {
+      //   console.log(x)
+      // });
+      console.log('No, not yet. Coming Soon.')
     }
   })
 }
