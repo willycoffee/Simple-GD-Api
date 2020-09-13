f3cca54834f46a8b7a9ed0a7562c32b58484a603
willycoffee <67596846+willycoffee@users.noreply.github.com>
2020-09-13 08:21:23 -0400

    now levels dont take like 3 years to load

--- level.js
+++ level.js
@@ -1,25 +1,23 @@
 const req = require("request");
 const atob = require("atob");
 
-function getLevel(levelID) {
-  req.post ("http://boomlings.com/database/downloadGJLevel22.php", { form: {secret: "Wmfd2893gb7",
-  levelID: levelID}}, (err, res, body) => {
-  let response = body.toString()
-  response = response.split(':')
+function getLevel(level) {
+  req.post("http://boomlings.com/database/getGJLevels21.php", {form: {secret: "Wmfd2893gb7", str: levelID, type: 0
+}}, (err, res, body) => {
+  body = body.split(':')
 
-  // let orbs = [0, 0, 50, 75, 125, 175, 225, 275, 350, 425, 500]
   let length = ['Tiny', 'Short', 'Medium', 'Long', 'XL']
-  let difficulty = { 0: 'Unrated', 10: 'Easy', 20: 'Normal', 30: 'Hard', 40: 'Harder', 50: 'Insane' }
+  let difficulty = { 0: 'Unrated', 10: 'Easy', 20: 'Normal', 30: 'Hard', 40: 'Harder', 50: 'Insane', 60: 'Demon'}
 
   const levelInfo = {
-    levelID: response[1],
-    levelName: response[3],
-    levelDescription: atob(response[5]) || "(No description provided)",
-    levelDownloads: response[17],
-    levelLikes: response[23],
-    levelUploaded: response[45],
-    levelUpdated: response[47],
-    levelDifficulty: difficulty[response[15]]
+    levelID: body[1],
+    levelName: body[3],
+    levelDescription: atob(body[35]) || "(No description provided)",
+    levelAuthor: body[54],
+    levelDownloads: body[13],
+    levelLikes: body[19]
+    // levelDifficulty: difficulty[body[9]],
+    // levelFeatured: body[29] || 0
   }
 
   if (body == '-1') {
@@ -30,4 +28,34 @@ function getLevel(levelID) {
 })
 }
 
-getLevel(128)
+// function getLevel(levelID) {
+//   req.post ("http://boomlings.com/database/downloadGJLevel22.php", { form: {secret: "Wmfd2893gb7",
+//   levelID: levelID}}, (err, res, body) => {
+//   let response = body.toString()
+//   response = response.split(':')
+//
+//   // let orbs = [0, 0, 50, 75, 125, 175, 225, 275, 350, 425, 500]
+//   let length = ['Tiny', 'Short', 'Medium', 'Long', 'XL']
+//   let difficulty = { 0: 'Unrated', 10: 'Easy', 20: 'Normal', 30: 'Hard', 40: 'Harder', 50: 'Insane' }
+//
+//   const levelInfo = {
+//     levelID: response[1],
+//     levelName: response[3],
+//     levelDescription: atob(response[5]) || "(No description provided)",
+//     levelDownloads: response[17],
+//     levelLikes: response[23],
+//     levelUploaded: response[45],
+//     levelUpdated: response[47],
+//     levelDifficulty: difficulty[response[15]],
+//     levelFeatured: response[29] || 0
+//   }
+//
+//   if (body == '-1') {
+//     console.log('Not found.')
+//   } else {
+//     console.log(levelInfo)
+//   }
+// })
+// }
+
+getLevelNew(42584142)
