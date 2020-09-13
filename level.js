f64eff9c9ef84c4a5d72256b4950e3d972a4753f
willycoffee <67596846+willycoffee@users.noreply.github.com>
2020-09-13 08:46:11 -0400

    removed the old level method completely to save code

--- level.js
+++ level.js
@@ -6,6 +6,9 @@ function getLevel(level) {
 }}, (err, res, body) => {
   body = body.split(':')
 
+  let length = ['Tiny', 'Short', 'Medium', 'Long', 'XL']
+  let difficulty = { 0: 'Unrated', 10: 'Easy', 20: 'Normal', 30: 'Hard', 40: 'Harder', 50: 'Insane', 60: 'Demon'}
+
   const levelInfo = {
     levelID: body[1],
     levelName: body[3],
@@ -24,35 +27,5 @@ function getLevel(level) {
   }
 })
 }
-
-// function getLevel(levelID) {
-//   req.post ("http://boomlings.com/database/downloadGJLevel22.php", { form: {secret: "Wmfd2893gb7",
-//   levelID: levelID}}, (err, res, body) => {
-//   let response = body.toString()
-//   response = response.split(':')
-//
-//   // let orbs = [0, 0, 50, 75, 125, 175, 225, 275, 350, 425, 500]
-//   let length = ['Tiny', 'Short', 'Medium', 'Long', 'XL']
-//   let difficulty = { 0: 'Unrated', 10: 'Easy', 20: 'Normal', 30: 'Hard', 40: 'Harder', 50: 'Insane' }
-//
-//   const levelInfo = {
-//     levelID: response[1],
-//     levelName: response[3],
-//     levelDescription: atob(response[5]) || "(No description provided)",
-//     levelDownloads: response[17],
-//     levelLikes: response[23],
-//     levelUploaded: response[45],
-//     levelUpdated: response[47],
-//     levelDifficulty: difficulty[response[15]],
-//     levelFeatured: response[29] || 0
-//   }
-//
-//   if (body == '-1') {
-//     console.log('Not found.')
-//   } else {
-//     console.log(levelInfo)
-//   }
-// })
-// }
 
 getLevelNew(42584142)
